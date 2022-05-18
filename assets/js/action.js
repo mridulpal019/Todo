fb8c33995b93ca6fe3d9de41a72b217e0ca37c6c
Mridul <mridulpal19@gmail.com>
2022-05-18 12:20:49 +0530

    add assets(css and js)

---
+++ assets/js/action.js
@@ -0,0 +1,1 @@
+console.log("hey there your action is working");
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('/',require("./routes/index"))
 app.set("view engine","ejs");
 app.set("views" ,"./views")
 
-
+app.use(express.static("assets"));//setting express to lock up assests for static files
 app.listen(port,function(err){
     if (err){
         console.log(`Error in running the server:${err}`);
